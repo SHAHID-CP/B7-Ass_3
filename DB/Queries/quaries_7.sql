48ffc91926e70740b74ff8d717c222fcd3566d4f
Black Fighter <blackfighter@gmail.com>
2026-06-15 16:58:53 +0600

    second query

--- DB/Queries/quaries_7.sql
+++ DB/Queries/quaries_7.sql
@@ -3,3 +3,9 @@ SELECT match_id, fixture, TRUNC(base_ticket_price) AS base_ticket_price
 FROM Matches 
 WHERE tournament_category = 'Champions League' AND match_status = 'Available'
 ORDER BY base_ticket_price DESC;
+
+-- Q2: Users search using by ILIKE
+SELECT user_id, full_name, email 
+FROM Users 
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
+ORDER BY user_id;

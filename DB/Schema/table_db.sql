d661b5be68437985b87e8569c4ab358136dd9a0d
Black Fighter <blackfighter@gmail.com>
2026-06-15 12:22:21 +0600

    user table make

---
+++ DB/Schema/table_db.sql
@@ -0,0 +1,17 @@
+-- User table-1
+CREATE TABLE Users (
+    user_id SERIAL,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    role VARCHAR(30) NOT NULL DEFAULT 'Football Fan',
+    phone_number VARCHAR(20),
+    
+    CONSTRAINT pk_users PRIMARY KEY (user_id),
+    CONSTRAINT uq_users_email UNIQUE (email),
+    CONSTRAINT chk_user_role CHECK (role IN ('Ticket Manager', 'Football Fan')),
+    CONSTRAINT chk_email_format CHECK (email LIKE '%_@__%.__%'),
+    CONSTRAINT chk_users_phone CHECK (
+            phone_number IS NULL
+            OR (phone_number ~ '^\+[0-9]{7,19}$')
+            )
+);

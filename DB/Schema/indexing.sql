ac4fc06b6ce9eda822dcd9b7fc753af0711dd1c5
Black Fighter <blackfighter@gmail.com>
2026-06-15 13:04:24 +0600

    matches table indexing

--- DB/Schema/indexing.sql
+++ DB/Schema/indexing.sql
@@ -1,2 +1,6 @@
--- user indexing
+-- user table indexing
 CREATE INDEX idx_users_email ON Users (email);
+
+-- matches table indexing
+CREATE INDEX idx_matches_status              ON Matches (match_status);
+CREATE INDEX idx_matches_tournament_category ON Matches (tournament_category);
